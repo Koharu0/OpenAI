5077cf7a44b79e913379ca71080afa8fb9a0ec28
Koharu0 <github@breeze.cat>
2024-08-15 05:20:10 +0900

    Update Text to speech.py

--- Text to speech.py
+++ Text to speech.py
@@ -3,10 +3,10 @@ from openai import OpenAI
 client = OpenAI()
 
 response = client.audio.speech.create(
-    model="tts-1-hd",
-    voice="nova",
+    model="tts-1-hd", #tts-1, tts-1-hd
+    voice="nova", #alloy, echo, fable, onyx, nova, shimmer
     input="Hello, World!",
 )
 
-with open("output.flac", "wb") as f:
+with open("output.flac", "wb") as f: #Opus, AAC, FLAC, WAV, PCM
     f.write(response.content)
